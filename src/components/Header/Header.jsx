c1d7b78d4f418ecce83402ad9d012d545a99d38b
Ian_França <iiann_2009@hotmail.com>
2023-06-22 19:19:20 -0300

    atualizado logica e estilo do componente Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,13 +5,14 @@ export default function Header() {
             <header>
                 <div className='container__flex'>
                     <img src="/src/assets/logo.svg" alt="logo Meteora" />
-                    <div>
-                        <button className='menu__button'></button>
+                    <div className='container__nav'>
+                        <button onClick={(event) => event.target.nextSibling.classList.toggle("open")} className='menu__button'></button>
                         <nav className='menu__nav'>
                             <ul>
-                                <li><a href="#">Home</a></li>
-                                <li><a href="#">Nossas lojas</a></li>
-                                <li><a href="#">Novidades</a></li>
+                                <div className='container__botao-fechar'><button onClick={(event) => event.target.parentNode.parentNode.parentNode.classList.toggle('open')} /></div>
+                                <li><a href="#">Home</a><p></p></li>
+                                <li><a href="#">Nossas lojas</a><p></p></li>
+                                <li><a href="#">Novidades</a><p></p></li>
                                 <li><a href="#">Promoções</a></li>
                             </ul>
                         </nav>
