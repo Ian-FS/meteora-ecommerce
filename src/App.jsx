695b362bd651fd17b5b814be4b6f977a35176f35
Ian_França <iiann_2009@hotmail.com>
2023-06-25 15:31:46 -0300

    atualizado lógica do componente App

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,17 @@
 import './App.css'
 import Category from './components/Category/Category'
 import Header from './components/Header/Header'
-<<<<<<< HEAD
 import Main from './components/Main/Main'
-=======
 import SearchBar from './components/SearchBar/SearchBar'
->>>>>>> 8d25b7f67a2bd642a03623f7bd1bda23e9d4e8f8
 
 function App() {
 
   return (
     <>
       <Header></Header>
-<<<<<<< HEAD
+      <SearchBar></SearchBar>
       <Main />
       <Category />
-=======
-      <SearchBar></SearchBar>
->>>>>>> 8d25b7f67a2bd642a03623f7bd1bda23e9d4e8f8
     </>
   )
 }
