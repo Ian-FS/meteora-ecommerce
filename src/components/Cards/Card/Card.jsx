a04f4ca8c8d05b19d1d8c5cdf1ab7f62e12b2dfa
Ian_França <iiann_2009@hotmail.com>
2023-06-27 03:13:46 -0300

    atualizado lógica e estilo do componente Card

--- src/components/Cards/Card/Card.jsx
+++ src/components/Cards/Card/Card.jsx
@@ -1,12 +1,17 @@
+/* eslint-disable react/prop-types */
+import './Card.scss'
+
 
 export default function Card(props) {
     return (
         <div className="card__container">
             <img src={props.img} alt="" />
-            <h3>{props.product}</h3>
-            <p>{props.description}</p>
-            <p>{props.value}</p>
-            <button>Ver mais</button>
+            <div className='info__container'>
+                <h3>{props.product}</h3>
+                <h4>{props.description}</h4>
+                <h3>{props.value}</h3>
+                <button>Ver mais</button>
+            </div>
         </div>
     )
 }
