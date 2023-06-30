eea3af33dbb61a5eac4bbf6f5a1f3dcc3cf43fc3
Ian_França <iiann_2009@hotmail.com>
2023-06-30 19:30:15 -0300

    responsividade para telas de tablet no componente Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,7 +12,21 @@ export default function Header() {
         <>
             <header>
                 <div className='container__flex'>
-                    <img src="/src/assets/logo.svg" alt="logo Meteora" />
+                    <div className='left__header'>
+                        <img src="/src/assets/logo.svg" alt="logo Meteora" />
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Nossas lojas</a></li>
+                            <li><a href="#">Novidades</a></li>
+                            <li><a href="#">Promoções</a></li>
+                        </ul>
+
+                    </div>
+                    <div className='search__header'>
+                        <input type="text" id='input-bar' placeholder='Digite o produto' />
+                        <button>Buscar</button>
+                    </div>
+
                     <div className='container__nav'>
                         <button onClick={handleToggle} className='menu__button'></button>
                         <nav className={isOpen ? 'menu__nav open' : 'menu__nav'}>
