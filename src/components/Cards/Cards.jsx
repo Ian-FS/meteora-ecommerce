472a18c5b57caddb0e62d02c27f01daafca02525
Ian_França <iiann_2009@hotmail.com>
2023-07-01 11:57:37 -0300

    atualizado a resposividade dos componentes Category, Cards e Card

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -17,42 +17,44 @@ export default function Cards() {
     return (
         <div className='cards__container'>
             <p>Produtos que estão bombando!</p>
-            <Card
-                img={camiseta}
-                product={'Camiseta Conforto'}
-                description={'Multicores e tamanhos. Tecido de algodão 100%, fresquinho para o verão. Modelagem unissex.'}
-                value={'R$ 70,00'}
-            />
-            <Card
-                img={calca}
-                product={'Calça Alfaiataria'}
-                description={'Modelo Wide Leg alfaiataria em linho. Uma peça pra vida toda!'}
-                value={'R$ 180,00'}
-            />
-            <Card
-                img={tenis}
-                product={'Tênis Chunky'}
-                description={'Snicker casual com solado mais alto e modelagem robusta. Modelo unissex.'}
-                value={'R$ 250,00'}
-            />
-            <Card
-                img={jaqueta}
-                product={'Jaqueta Jeans'}
-                description={'Modelo unissex oversized com gola de camurça. Atemporal e autêntica!'}
-                value={'R$ 150,00'}
-            />
-            <Card
-                img={oculos}
-                product={'Óculos Redondo'}
-                description={'Armação metálica em grafite com lentes arredondadas. Sem erro!'}
-                value={'R$ 120,00'}
-            />
-            <Card
-                img={bolsa}
-                product={'Bolsa coringa'}
-                description={'Bolsa camel em couro sintético de alta duração. Ideal para acompanhar você por uma vida!'}
-                value={'R$ 120,00'}
-            />
+            <div className='grid__container'>
+                <Card
+                    img={camiseta}
+                    product={'Camiseta Conforto'}
+                    description={'Multicores e tamanhos. Tecido de algodão 100%, fresquinho para o verão. Modelagem unissex.'}
+                    value={'R$ 70,00'}
+                />
+                <Card
+                    img={calca}
+                    product={'Calça Alfaiataria'}
+                    description={'Modelo Wide Leg alfaiataria em linho. Uma peça pra vida toda!'}
+                    value={'R$ 180,00'}
+                />
+                <Card
+                    img={tenis}
+                    product={'Tênis Chunky'}
+                    description={'Snicker casual com solado mais alto e modelagem robusta. Modelo unissex.'}
+                    value={'R$ 250,00'}
+                />
+                <Card
+                    img={jaqueta}
+                    product={'Jaqueta Jeans'}
+                    description={'Modelo unissex oversized com gola de camurça. Atemporal e autêntica!'}
+                    value={'R$ 150,00'}
+                />
+                <Card
+                    img={oculos}
+                    product={'Óculos Redondo'}
+                    description={'Armação metálica em grafite com lentes arredondadas. Sem erro!'}
+                    value={'R$ 120,00'}
+                />
+                <Card
+                    img={bolsa}
+                    product={'Bolsa coringa'}
+                    description={'Bolsa camel em couro sintético de alta duração. Ideal para acompanhar você por uma vida!'}
+                    value={'R$ 120,00'}
+                />
+            </div>
         </div>
     )
 }
