ef06d9a9335c706e7e7efac151ed9a6639e6f2f1
Ian_França <iiann_2009@hotmail.com>
2023-07-02 12:04:36 -0300

    atualizado estilo do componente Main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,15 @@
-import carrosselImg from '../../assets/carrossel.svg'
+import carrosselMobile from '../../assets/carrossel.svg'
+import carrosselTablet from '../../assets/carousel _ 768.svg'
+import carrosselWeb from '../../assets/carousel _ 1440.svg'
 import './Main.scss'
 
 export default function Main() {
     return (
         <>
             <div className="carrossel__container">
-                <img src={carrosselImg} alt="imagem de " />
+                <img className='img__mobile' src={carrosselMobile} alt="imagem de moda" />
+                <img className='img__tablet' src={carrosselTablet} alt="imagem de moda" />
+                <img className='img__web' src={carrosselWeb} alt="imagem de moda" />
             </div>
         </>
     )
