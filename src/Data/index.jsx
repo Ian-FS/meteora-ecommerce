5ac8d3477715b5bf8895e2a22a60e360f73bb1e3
Ian_França <iiann_2009@hotmail.com>
2023-07-14 20:35:03 -0300

    atualizado caminho das imagens dos itens

--- src/Data/index.jsx
+++ src/Data/index.jsx
@@ -1,37 +1,37 @@
 export const listOfProduct = [
     {
         product: 'Camiseta Conforto',
-        image: '../assets/items/item-camiseta.svg',
+        image: '../../src/assets/items/item-camiseta.svg',
         description: 'Multicores e tamanhos. Tecido de algodão 100%, fresquinho para o verão. Modelagem unissex.',
         value: 'R$ 70,00',
     },
     {
         product: 'Calça Alfaiataria',
-        image: '../assets/items/item-calca.svg',
+        image: '../../src/assets/items/item-calca.svg',
         description: 'Modelo Wide Leg alfaiataria em linho. Uma peça pra vida toda!',
         value: 'R$ 180,00',
     },
     {
         product: 'Tênis Chunky',
-        image: '../assets/items/item-tenis.svg',
+        image: '../../src/assets/items/item-tenis.svg',
         description: 'Snicker casual com solado mais alto e modelagem robusta. Modelo unissex.',
         value: 'R$ 250,00',
     },
     {
         product: 'Jaqueta Jeans',
-        image: '../assets/items/item-jaqueta-jeans.svg',
+        image: '../../src/assets/items/item-jaqueta-jeans.svg',
         description: 'Modelo unissex oversized com gola de camurça. Atemporal e autêntica!',
         value: 'R$ 150,00',
     },
     {
         product: 'Óculos Redondo',
-        image: '../assets/items/item-oculos.svg',
+        image: '../../src/assets/items/item-oculos.svg',
         description: 'Armação metálica em grafite com lentes arredondadas. Sem erro!',
         value: 'R$ 120,00',
     },
     {
         product: 'Bolsa coringa',
-        image: '../assets/items/item-bolsa.svg',
+        image: '../../src/assets/items/item-bolsa.svg',
         description: 'Bolsa camel em couro sintético de alta duração. Ideal para acompanhar você por uma vida!',
         value: 'R$ 120,00',
     }
