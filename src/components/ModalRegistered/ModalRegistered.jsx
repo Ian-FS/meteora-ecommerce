b06103c3826e3e1666ecfb1f4220b79ac59ecb83
Ian_França <iiann_2009@hotmail.com>
2023-07-17 11:42:19 -0300

    atualizado a estrutura e lógica do componente ModalRegistered

--- src/components/ModalRegistered/ModalRegistered.jsx
+++ src/components/ModalRegistered/ModalRegistered.jsx
@@ -1,20 +1,26 @@
+/* eslint-disable react/prop-types */
 import './ModalRegistered.scss'
 import checkCircle from '../../assets/View-More/check-circle.svg'
 import iconClose from '../../assets/View-More/Icon-Close.svg'
 
 export default function ModalRegistered({ setIsOpen }) {
     return (
-        <div>
-            <header className='wrapper__header-modal'>
-                <div className='title__header'>
-                    <img src={checkCircle} alt="" />
-                    <h2>E-mail cadastrado com sucesso!</h2>
-                </div>
-                <div className='close__header'>
-                    <img onClick={() => setIsOpen(false)} src={iconClose} alt="" />
-                </div>
-            </header>
-            <main></main>
+        <div className='background__modal'>
+            <div className='wrapper__modal'>
+                <header className='wrapper__header-modal'>
+                    <div className='title__header'>
+                        <img src={checkCircle} alt="" />
+                        <h2>E-mail cadastrado com sucesso!</h2>
+                    </div>
+                    <div className='close__header'>
+                        <img onClick={() => setIsOpen(false)} src={iconClose} alt="" />
+                    </div>
+                </header>
+                <main className='modal__content'>
+                    Em breve você receberá novidades exclusivas da Meteora.
+                </main>
+            </div>
         </div>
+
     )
 }
