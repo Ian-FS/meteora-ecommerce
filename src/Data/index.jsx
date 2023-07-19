7b5eb38c613c93e6f2773672226afb2b96d21bd3
Ian_França <iiann_2009@hotmail.com>
2023-07-19 13:24:34 -0300

    atualizado caminho das imagens das roupas

--- src/Data/index.jsx
+++ src/Data/index.jsx
@@ -1,42 +1,50 @@
+import camiseta from '../assets/items/item-camiseta.svg'
+import calca from '../assets/items/item-calca.svg'
+import tenis from '../assets/items/item-tenis.svg'
+import jaqueta from '../assets/items/item-jaqueta-jeans.svg'
+import oculos from '../assets/items/item-oculos.svg'
+import bolsa from '../assets/items/item-bolsa.svg'
+
+
 export const listOfProduct = [
     {
         name: 'Camiseta Conforto',
-        image: '../assets/items/item-camiseta.svg',
+        image: camiseta,
         description: 'Multicores e tamanhos. Tecido de algodão 100%, fresquinho para o verão. Modelagem unissex.',
         value: 'R$ 70,00',
         type: 'camiseta',
     },
     {
         name: 'Calça Alfaiataria',
-        image: '../',
+        image: calca,
         description: 'Modelo Wide Leg alfaiataria em linho. Uma peça pra vida toda!',
         value: 'R$ 180,00',
         type: 'calça',
     },
     {
         name: 'Tênis Chunky',
-        image: '../assets/items/item-tenis.svg',
+        image: tenis,
         description: 'Snicker casual com solado mais alto e modelagem robusta. Modelo unissex.',
         value: 'R$ 250,00',
         type: 'calçado',
     },
     {
         name: 'Jaqueta Jeans',
-        image: '../assets/items/item-jaqueta-jeans.svg',
+        image: jaqueta,
         description: 'Modelo unissex oversized com gola de camurça. Atemporal e autêntica!',
         value: 'R$ 150,00',
         type: 'casaco',
     },
     {
         name: 'Óculos Redondo',
-        image: '/src/assets/items/item-oculos.svg',
+        image: oculos,
         description: 'Armação metálica em grafite com lentes arredondadas. Sem erro!',
         value: 'R$ 120,00',
         type: 'oculos',
     },
     {
         name: 'Bolsa coringa',
-        image: '/src/assets/items/item-bolsa.svg',
+        image: bolsa,
         description: 'Bolsa camel em couro sintético de alta duração. Ideal para acompanhar você por uma vida!',
         value: 'R$ 120,00',
         type: 'bolsa',
